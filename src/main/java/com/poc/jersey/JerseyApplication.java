0a6e91aa0b434bdd5ebabfdca61817bf78052f7e
Sundara Balaji J K <sundarabalajijk@gmail.com>
2018-05-20 12:59:47 +0530

    input stream fixed

--- src/main/java/com/poc/jersey/JerseyApplication.java
+++ src/main/java/com/poc/jersey/JerseyApplication.java
@@ -2,6 +2,9 @@ package com.poc.jersey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.filter.OrderedRequestContextFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.RequestContextFilter;
 
 @SpringBootApplication
 public class JerseyApplication {
@@ -9,4 +12,12 @@ public class JerseyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JerseyApplication.class, args);
 	}
+
+	@Bean
+	public RequestContextFilter requestContextFilter() {
+		OrderedRequestContextFilter filter = new OrderedRequestContextFilter();
+		filter.setOrder(-100001);
+		return filter;
+	}
 }
+
